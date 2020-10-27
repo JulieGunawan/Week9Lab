d03a3e3d265f14858f437019bc26bf9374ebbdb7
PC <PC@DESKTOP-SEUQDOV.hitronhub.home>
2020-10-27 11:40:15 -0600

    UserServlet Skeleton code

--- Week8Lab/src/java/servlets/UserServlet.java
+++ Week8Lab/src/java/servlets/UserServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class UserServlet extends HttpServlet {
 
@@ -14,6 +15,13 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+         //Initialize Services here
+        HttpSession session = request.getSession();
+        //List<Users> users = us.getAll(email);
+        //List<Users> users = us.getAll(firstname);
+        //List<Users> users = us.getAll(lastname);
+        //request.setAttribute("users", users);
        
     }
 
@@ -21,6 +29,29 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession session = request.getSession();
+       //Initialize Services here
+       
+        String action = request.getParameter("action");
+        String email = request.getParameter("email");
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String password = request.getParameter("password");
+        String active = request.getParameter("active");
+        String role = request.getParameter("role");
+        
+        switch (action) {
+            case "add":
+                //add user
+                break;
+            case "update":
+                //update user
+                break;
+            case "delete":
+                //delete user
+                break;
+                   
+        }
         
     }
 
