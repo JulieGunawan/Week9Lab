b90c97e8998259aec58909354f2fe223b404f3d2
760483 <760483@SAIT224160IT.ACDM.DS.SAIT.CA>
2020-10-27 10:30:01 -0600

    Created dataaccess package.

---
+++ Week8Lab/src/java/dataaccess/ConnectionPool.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess;
+
+/**
+ *
+ * @author 760483
+ */
+public class ConnectionPool {
+    private static ConnectionPool pool = null;
+    //private static DataSource dataSource = null;
+}
