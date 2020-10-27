d05389537b6f2b8fd233ea899325e378f080d7e0
815000 <815000@SAIT224318IT.mshome.net>
2020-10-27 11:18:41 -0600

    Implemented RoleDB.java

---
+++ Week8Lab/src/java/dataaccess/RoleDB.java
@@ -0,0 +1,38 @@
+package dataaccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleDB {
+
+    public List<Role> getAll() throws Exception {
+        List<Role> roles = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT * FROM role";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int roleId = rs.getInt(1);
+                String roleName = rs.getString(2);
+                Role role = new Role(roleId, roleName);
+                roles.add(role);
+            }
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+
+        return notes;
+    }
+
+}
